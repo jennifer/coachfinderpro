0aa20eafed9f7d65194d299807791b8d8453b0e2
jennifer kerssen <jjkerssen@gmail.com>
2019-09-29 13:02:36 -0600

    remove pug refs

--- app.js
+++ app.js
@@ -6,19 +6,15 @@ var logger = require('morgan');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// app.post('/', function (req, res) {
+//   res.send('Got a POST request')
+// })
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
