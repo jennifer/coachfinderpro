ece567842efce98378760140dd02b47b362c3b19
jennifer kerssen <jjkerssen@gmail.com>
2019-09-29 13:39:42 -0600

    update buildpack

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-fetch('https://coach-finder.herokuapp.com/api/v1/parts/')
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
+// fetch('https://coach-finder.herokuapp.com/api/v1/parts/')
+//   .then(
+//     function(response) {
+//       if (response.status !== 200) {
+//         console.log('Looks like there was a problem. Status Code: ' +
+//           response.status);
+//         return;
+//       }
 
-      // Examine the text in the response
-      response.json().then(function(data) {
-        console.log(data);
-      });
-    }
-  )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
+//       // Examine the text in the response
+//       response.json().then(function(data) {
+//         console.log(data);
+//       });
+//     }
+//   )
+//   .catch(function(err) {
+//     console.log('Fetch Error :-S', err);
+//   });
