1f87196f9d34796ef15b3a31dcf6791efd7bda25
jennifer kerssen <jjkerssen@gmail.com>
2019-10-05 17:13:42 -0600

    add show more results button

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 function getDataFromApi(event) {
   event.preventDefault();
-  let searchInput = document.getElementById('searchinput').value;
   const oldResults = document.getElementById("results");
   while (oldResults.firstChild) {
     oldResults.removeChild(oldResults.firstChild);
   }
+  let searchInput = document.getElementById('searchinput').value;
   if (searchInput) {
     fetch(`https://coach-finder.herokuapp.com/api/v1/parts/search?q=${searchInput}`)
     .then(
@@ -14,7 +14,7 @@ function getDataFromApi(event) {
           return;
         }
         response.json().then(function(data) {
-          renderResults(data, searchInput)
+          renderResultInfo(data, searchInput)
         });
         document.getElementById('searchinput').value = '';
       }
@@ -25,32 +25,61 @@ function getDataFromApi(event) {
   }
   else {
     const searchAgain = document.createElement('p');
-    searchAgain.textContent = "Enter a Name, Color, 0r Style to Search"
+    searchAgain.textContent = "Enter a Name, Color, or Style to Search"
     results.appendChild(searchAgain);
   }
 }
 
-function renderResults(data, searchInput) {
+function renderResultInfo(data, searchInput) {
   if (data && data.length) {
-    const resultsInfo = document.getElementById('resultsInfo');
-    resultsInfo.textContent = `${data.length}` + ' results for ' + `${searchInput}` + ':';
-    data.forEach(renderEachResult);
-    function renderEachResult(eachResult, index) {
-      const results = document.getElementById('results');
-      const catalogPage = document.createElement('li');
-      const nameAndDate = document.createElement('p');
-      const image = document.createElement('img');
-      nameAndDate.textContent = `${data[index].catalog_name}` + " " +`${data[index].year}`;
-      image.src=`${data[index].img_url}`;
-        
-      results.appendChild(catalogPage);
-      catalogPage.appendChild(nameAndDate);
-      catalogPage.appendChild(image);
+    const resultInfo = document.getElementById('resultInfo');
+    resultInfo.textContent = `${data.length}` + ' results for ' + `${searchInput}` + ':';
+    if (data.length <= 9) {
+      renderResults(data);
     }
+    else {renderSubArray(data)};
   }
   else {
     const searchAgain = document.createElement('p');
     searchAgain.textContent = "No results. Try a different search."
     results.appendChild(searchAgain);
+  }
+}
+
+let dataArr;
+let currentPage = 1;
+let startIndex = 0;
+let endIndex = 9;
+function renderSubArray(data) {
+  dataArr = data;
+  const pageCount = Math.ceil(dataArr.length / 9);
+  const subArray = data.slice(startIndex, endIndex);
+  renderResults(subArray);
+  if (currentPage < pageCount) {
+    document.getElementById("loadmore").style.visibility = "visible";
+    currentPage ++;
+    startIndex += 9;
+    endIndex += 9;
+  }
+  else {
+    document.getElementById("loadmore").style.visibility = "hidden";
+    currentPage = 1;
+    startIndex = 0;
+    endIndex = 9;
+  }
+}
+
+function renderResults(data) {
+  data.forEach(renderEachResult);
+  function renderEachResult(eachResult, index) {
+    const results = document.getElementById('results');
+    const catalogPage = document.createElement('li');
+    const nameAndDate = document.createElement('p');
+    const image = document.createElement('img');
+    nameAndDate.textContent = `${data[index].catalog_name}` + " " +`${data[index].year}`;
+    image.src=`${data[index].img_url}`;
+    results.appendChild(catalogPage);
+    catalogPage.appendChild(nameAndDate);
+    catalogPage.appendChild(image);
   }
 }
